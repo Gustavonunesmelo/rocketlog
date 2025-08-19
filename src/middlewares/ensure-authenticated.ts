a89892085293da3206a8e533f169776db420027e
Gustavo nunes melo <156378462+Gustavonunesmelo@users.noreply.github.com>
2025-08-19 13:46:37 -0300

    add authentication middleware and extend Express Request type

---
+++ src/middlewares/ensure-authenticated.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+import { authConfig } from "@/configs/auth";
+import { AppError } from "@/utils/AppError";
+
+interface TokenPayload {
+  sub: string;
+  role: string;
+}
+
+function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("JWT token is missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub, role } = verify(token, authConfig.jwt.secret) as TokenPayload;
+
+    req.user = {
+      id: sub,
+      role,
+    };
+
+    return next();
+  } catch {
+    throw new AppError("Invalid JWT token", 401);
+  }
+}

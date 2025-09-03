695926b3b2126d9f9a8fe2c80462e104d685c49d
Gustavo nunes melo <156378462+Gustavonunesmelo@users.noreply.github.com>
2025-09-03 14:20:06 -0300

    feat: include delivery logs and user details in delivery retrieval

--- src/controllers/deliveries-logs-controller.ts
+++ src/controllers/deliveries-logs-controller.ts
@@ -42,6 +42,10 @@ class DeliveryLogsController {
 
     const delivery = await prisma.delivery.findUnique({
       where: { id: delivery_id },
+      include: { 
+        DeliveryLog: true,
+        user: true
+      },
     });
 
     if (req.user?.role === "customer" && req.user.id !== delivery?.userId) {
